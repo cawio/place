a8f8085b3a826599b9ca5a7f2bd0203ed1e4dd22
Simon <sberner9@gmail.com>
2024-03-09 11:54:57 +0100

    feat: :sparkles: Create Projects

---
+++ Place.Data/DTOs/CellDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Place.Data;
+
+public class CellDTO
+{
+    public string Id { get; set; } = string.Empty;
+    public int X { get; set; }
+    public int Y { get; set; }
+    public string? Color { get; set; }
+}
---
+++ Place.Data/Models/Cell.cs
@@ -0,0 +1,9 @@
+﻿namespace Place.Data;
+
+public class Cell
+{
+    public int Id { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public string? Color { get; set; }
+}
